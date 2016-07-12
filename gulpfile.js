7ee3927f9843a1c668f1140d0c3f52ad4da7ee2e
nedzen <nedzen.11@gmail.com>
2016-07-12 15:35:58 +0200

    edited gulpfile and added inludes to gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ gulp.task('browserSync', function() {
 
 gulp.task('watch', ['browserSync', 'sass', 'jade'], function (){
 	gulp.watch('src/sass/**/*.sass', ['sass']); 
-	gulp.watch('src/templates/*.jade', ['jade']);
+	gulp.watch('src/templates/**/*.jade', ['jade']);
 	gulp.watch('dist/*.html', browserSync.reload);
 });
 
